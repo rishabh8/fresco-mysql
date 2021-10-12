e753392a7001940e5fc3d3af0ccd025880f90c9b
LabUser <labuser@ip-172-31-25-49.ap-south-1.compute.internal>
2021-10-12 18:51:46 +0000

    sample

--- src/main/java/com/fresco/jdbc/code/DbOperations.java
+++ src/main/java/com/fresco/jdbc/code/DbOperations.java
@@ -13,10 +13,25 @@ import com.fresco.jdbc.code.util.DbUtil;
 
 public class DbOperations {
 	Connection con;
+	ResultSet res;
 	public DbOperations() {
 		con = DbUtil.getConnection();
 	}
 	public boolean insertSubject(String name) throws SQLException {
+		PreparedStatement getIdValue = con.preparedStatement('select id from subject order by desc limit 1');
+		res = getIdValue.executeQuery();
+		res.next();
+		int subId = res.getInt(1);
+
+		PreparedStatement stat = con.preparedStatement('insert into subject values(?,?)');
+		stat.setInt(subId+1);
+		stat.setString('Maths');
+		stat.executeUpdate();
+
+		stat.close();
+		rs.close();
+		conn.close();
+
 		return false;
 	}
 	public ArrayList getSubjectById(int id) throws SQLException {
